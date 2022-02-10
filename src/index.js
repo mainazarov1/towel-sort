aa2d30d9b5a784667637420f150c789e156cc4ef
mainazarov1 <mainazarov1@gmail.com>
2022-02-10 08:05:22 +0600

    refactor: rewrite fn towelSort

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 // You should implement your task here.
 
+// module.exports = function towelSort(matrix) {
+//     let arr = [];
+//     if (matrix !== undefined) {
+//         for (let i = 0; i < matrix.length; i++) {
+//             if (i % 2 !== 0) {
+//                 matrix[i].reverse();
+//             }
+//             arr.push(...matrix[i]);
+//         }
+//     }
+//     return arr;
+// };
 module.exports = function towelSort(matrix) {
     let arr = [];
-    if (matrix === undefined) {
-        return arr;
-    }
-    for (let i = 0; i < matrix.length; i++) {
-        if (i % 2 !== 0) {
-            matrix[i].reverse();
-        }
-        arr.push(...matrix[i]);
+    if (matrix !== undefined) {
+        matrix.map((el, i) => {
+            if (i % 2 !== 0) {
+                el.reverse();
+            }
+            arr.push(...el);
+        });
     }
     return arr;
 };
